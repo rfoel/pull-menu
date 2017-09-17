dcd61a020849975e24cb220e78fc8758acb940ca
Rafael Franco <rafaelfr@outlook.com>
2017-09-17 01:54:05 -0300

    Added function to sort data

--- src/pull-menu.js
+++ src/pull-menu.js
@@ -2,11 +2,14 @@ const screen = document.querySelector("#screen")
 const nav = screen.querySelector("nav")
 const pullIcon = nav.querySelector(".pull-icon")
 const menu = nav.querySelector("#menu")
-const items = menu.querySelectorAll("li")
+const items = [...menu.querySelectorAll("li")]
 let mouseStartY
 const navHeight = nav.offsetHeight
 let isMouseDown
-const maxPull = 100
+const maxPull = 80
+let itemsCount
+let pullStep
+let index
 
 items.forEach((item, index) => {
   item.setAttribute("data-index", index)
@@ -14,6 +17,9 @@ items.forEach((item, index) => {
     menu.style.setProperty("transform", `translate(${getItemX(index)}px, 0)`)
   }
 })
+
+itemsCount = items.length
+pullStep = maxPull / (itemsCount - 1)
 
 function getItemX(index) {
   let item = menu.querySelector(`[data-index="${index}"]`)
@@ -24,30 +30,41 @@ function getItemX(index) {
   )
 }
 
-function pullRelease() {
+function pullEnd() {
   isMouseDown = false
   nav.style.height = `${navHeight}px`
   pullIcon.classList.remove("hide")
   menu.classList.remove("active")
-  menu.style.paddingTop = 0  
+  menu.style.paddingTop = 0
 }
 
 function pulling(e) {
   e.preventDefault()
   if (isMouseDown) {
     let newHeight = Math.max(0, e.offsetY - mouseStartY)
-    if (newHeight > maxPull)
+    if (newHeight > maxPull) {
       newHeight = maxPull + (newHeight - maxPull) / (newHeight * 0.01)
+    }
+    nav.style.height = `${navHeight + newHeight}px`
+
+    index = Math.floor(newHeight / pullStep)
+    if (index < itemsCount) {
+      let item = menu.querySelector(`[data-index="${index}"]`)
+      menu.style.setProperty("transform", `translate(${getItemX(index)}px, 0)`)
+      items.forEach(item => item.classList.remove("active"))
+      item.classList.add("active")
+      items.sort((x, y) => x.classList.contains("active") ? -1 : 1)
+      console.log(items)
+    }
 
     if (newHeight > maxPull - navHeight) {
       menu.classList.add("active")
       pullIcon.classList.add("hide")
-      menu.style.paddingTop = `${(newHeight - navHeight)/2}px`
+      menu.style.paddingTop = `${(newHeight - navHeight) / 1.7}px`
     } else {
       menu.classList.remove("active")
       pullIcon.classList.remove("hide")
     }
-    nav.style.height = `${navHeight + newHeight}px`
   }
 }
 
@@ -57,6 +74,6 @@ function pullStart(e) {
 }
 
 nav.addEventListener("mousedown", pullStart)
-screen.addEventListener("mouseup", pullRelease)
-screen.addEventListener("mouseleave", pullRelease)
+screen.addEventListener("mouseup", pullEnd)
+screen.addEventListener("mouseleave", pullEnd)
 screen.addEventListener("mousemove", pulling)
