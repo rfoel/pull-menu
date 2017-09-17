68e4dcb0594040d2ac583bb1bacb7b1817fc2442
Rafael Franco <rafaelfr@outlook.com>
2017-09-17 00:55:06 -0300

    Cleaned functions

--- src/pull-menu.js
+++ src/pull-menu.js
@@ -1,23 +1,22 @@
 const screen = document.querySelector("#screen")
-const nav = document.querySelector("nav")
-const menu = document.querySelector("#menu")
+const nav = screen.querySelector("nav")
+const pullIcon = nav.querySelector(".pull-icon")
+const menu = nav.querySelector("#menu")
 const items = menu.querySelectorAll("li")
-let isMouseDown
 let mouseStartY
-let maxPull = 56
-
-items[0].classList.add("active")
+const navHeight = nav.offsetHeight
+let isMouseDown
+const maxPull = 100
 
 items.forEach((item, index) => {
   item.setAttribute("data-index", index)
+  if (item.classList.contains("active")) {
+    menu.style.setProperty("transform", `translate(${getItemX(index)}px, 0)`)
+  }
 })
 
-menu.style.setProperty("transform", `translate(${getItemX(0)}px, 0)`)
-
-getItemX(0)
-
 function getItemX(index) {
-  let item = menu.querySelector(`[data-index='${index}']`)
+  let item = menu.querySelector(`[data-index="${index}"]`)
   let screenWidth = document.querySelector("#screen").offsetWidth
 
   return (
@@ -27,25 +26,37 @@ function getItemX(index) {
 
 function pullRelease() {
   isMouseDown = false
-  nav.style.height = "56px"
+  nav.style.height = `${navHeight}px`
+  pullIcon.classList.remove("hide")
+  menu.classList.remove("active")
+  menu.style.paddingTop = 0  
 }
 
-function pull(e) {
+function pulling(e) {
   e.preventDefault()
-  console.log(e.offsetY, mouseStartY)
   if (isMouseDown) {
-    let diff = Math.max(0, e.offsetY + mouseStartY)
-    if (diff > maxPull) {
-      nav.style.height = `${56 + (diff - maxPull) / (diff * 0.01)}px`
+    let newHeight = Math.max(0, e.offsetY - mouseStartY)
+    if (newHeight > maxPull)
+      newHeight = maxPull + (newHeight - maxPull) / (newHeight * 0.01)
+
+    if (newHeight > maxPull - navHeight) {
+      menu.classList.add("active")
+      pullIcon.classList.add("hide")
+      menu.style.paddingTop = `${(newHeight - navHeight)/2}px`
+    } else {
+      menu.classList.remove("active")
+      pullIcon.classList.remove("hide")
     }
+    nav.style.height = `${navHeight + newHeight}px`
   }
 }
 
-screen.addEventListener("mousedown", e => {
+function pullStart(e) {
   isMouseDown = true
   mouseStartY = e.offsetY
-})
+}
 
+nav.addEventListener("mousedown", pullStart)
 screen.addEventListener("mouseup", pullRelease)
 screen.addEventListener("mouseleave", pullRelease)
-screen.addEventListener("mousemove", pull)
+screen.addEventListener("mousemove", pulling)
